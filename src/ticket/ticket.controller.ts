886bf7286e598c04be34c67bd3b8dbf1a95c5f02
Djallel <zerroug.djallel@gmail.com>
2021-02-17 21:25:36 +0100

    controller return corrected

--- src/ticket/ticket.controller.ts
+++ src/ticket/ticket.controller.ts
@@ -3,7 +3,6 @@ import { Body, Controller, Post, Req } from "@nestjs/common";
 import { TicketService } from "./ticket.service";
 
 @Controller('ticket')
-//@Controller('ticket')
 export class TicketController {
   constructor(private ticketsService: TicketService) {}
 
@@ -13,19 +12,23 @@ export class TicketController {
 
       // Le corps est ignoré par NestJS -> Il faut obtenir le body brut à partir de la requête
       const rawData = await rawbody(req);
+
       // Conversion en string puis suppression des espaces blancs aux extrémités
       const ticketString = rawData.toString().trim();
 
       // Split du payload pour récupérer la partie ticket et la partie produit dans un tableau
       let ticketParts: string[] = ticketString.split('\r\n\r\n');
+
       // Partie attributs du ticket
       let ticketAttributes: string = ticketParts[0];
+
       // Partie produits du ticket
       let ticketProducts: string = ticketParts[1];
 
       try {
-        console.log(this.ticketsService.processTicket(ticketAttributes, ticketProducts));
+        return this.ticketsService.processTicket(ticketAttributes, ticketProducts);
       } catch (error) {
+        this.ticketsService.processRawTicket(rawData.toString());
         return(error);
       }
     }
